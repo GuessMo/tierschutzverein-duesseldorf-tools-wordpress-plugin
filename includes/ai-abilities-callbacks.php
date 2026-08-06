8f12e886eae6e0405aa7d4f85942206ccf2e2641
GuessMo <7084139+GuessMo@users.noreply.github.com>
2026-08-06 14:33:41 +0200

    feat: allow klaro/v1 routes in rest-request allowlist

--- includes/ai-abilities-callbacks.php
+++ includes/ai-abilities-callbacks.php
@@ -715,7 +715,7 @@ function tsvd_tools_ai_rest_request($input) {
         '/wp/v2/posts', '/wp/v2/pages', '/wp/v2/media', '/wp/v2/blocks',
         '/wp/v2/categories', '/wp/v2/tags', '/wp/v2/taxonomies', '/wp/v2/types',
         '/wp/v2/comments', '/wp/v2/users', '/wp/v2/settings', '/wp/v2/search',
-        '/wp/v2/animals', '/tsv-tools/',
+        '/wp/v2/animals', '/tsv-tools/', '/klaro/v1/',
     ));
     $route_path = strtok($route, '?');
     $allowed = false;
